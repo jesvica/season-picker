870cd44f8117fef40a40d6e8923214570163a23b
jessica <jvictoriaw@hotmail.co.uk>
2023-01-19 21:09:15 +0000

    changed buttons and added background selector

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+// document.querySelector('#check').addEventListener('click')
 
+function changeBackground() {
+    document.querySelector("#winter").style.backgroundColor = blue;
+}
+
+
+/*function season() {
+    document.getElementById("myDIV").style.backgroundImage = "url(Winter_Branch_4K_Living_Background.mp4)";
+ }*/
